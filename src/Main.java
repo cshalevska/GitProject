d7ab3b6387f3faa0adcfd48652bd7463d653117d
Cece Shalevska <cshalevska@gmail.com>
2022-05-14 12:08:35 -0400

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am java master");
+        System.out.println("I am not java master");
+        System.out.println("I am java basic learner");
+    }
+}
